d9c109fd2994c8e9edfe5446bb4f94e74a392744
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-10-25 02:09:34 -0300

    Initial commit

---
+++ Assets/Scripts/CameraPC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPC : MonoBehaviour
+{
+    private float _mouseYRotation = 0;
+    private Quaternion _originalRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _originalRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCameraUpAndDown();
+    }
+
+    private void RotateCameraUpAndDown()
+    {
+        _mouseYRotation += Input.GetAxis("Mouse Y");
+        _mouseYRotation = Mathf.Clamp(_mouseYRotation, -50, 50);
+        
+        Quaternion upDown = Quaternion.AngleAxis(_mouseYRotation, Vector3.left);
+        transform.localRotation = _originalRotation * upDown;
+    }
+}
---
+++ Assets/Scripts/PCController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PCController : MonoBehaviour
+{
+    private Rigidbody _rdb;
+    private Quaternion _originalRotation;
+    private float _mouseXRotation = 0;
+
+    public float velocity = 5;
+    public float velocityRotation = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rdb = GetComponent<Rigidbody>();
+        _originalRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.MovePC();
+        this.RotatePC();
+    }
+
+    private void MovePC()
+    {
+        float moveFoward = Input.GetAxis("Vertical");
+        float moveSide = Input.GetAxis("Horizontal");
+
+        Vector3 v3Velocity = new Vector3(
+            moveSide * velocity, _rdb.velocity.y, moveFoward * velocity
+        );
+        _rdb.velocity = transform.TransformDirection(v3Velocity);
+    }
+
+    private void RotatePC()
+    {
+        // Using keyboard Q and E
+        int keyboardRotation = 0;
+        if (Input.GetKey(KeyCode.Q))
+            keyboardRotation = -1;
+        else if (Input.GetKey(KeyCode.E))
+            keyboardRotation = 1;
+
+        transform.Rotate(new Vector3(
+            0, keyboardRotation * Time.deltaTime * velocityRotation, 0
+        ));
+        
+        // Using mouse
+        _mouseXRotation += Input.GetAxis("Mouse X");
+        Quaternion side = Quaternion.AngleAxis(_mouseXRotation, Vector3.up);
+        transform.localRotation = _originalRotation * side;
+    }
+}
