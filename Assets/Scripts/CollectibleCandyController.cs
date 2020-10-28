3e3dadf5fd22745a3fe207991301f481cdd017df
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-10-27 22:58:43 -0300

    Add candies all over the map

--- Assets/Scripts/CollectibleCandyController.cs
+++ Assets/Scripts/CollectibleCandyController.cs
@@ -8,7 +8,7 @@ public class CollectibleCandyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(new Vector3(-54.346f,-60.822f,85.83301f));
     }
 
     private void OnTriggerEnter(Collider other)
