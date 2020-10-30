b0a54ae4a92976fc94492d986e7a991256fd344a
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-10-30 00:02:51 -0300

    Add Portal

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    private Transform _cameraPosition;
+
+    public GameObject pc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraPosition = this.gameObject.transform.GetChild(0).gameObject.transform;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            pc.transform.position = _cameraPosition.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
